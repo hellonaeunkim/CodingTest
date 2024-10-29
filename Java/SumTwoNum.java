197bb7cc7cbbcf91518887fa354f9b9f1583f37f
Ann Kim <hellonaeunkim@gmail.com>
2024-10-29 21:05:54 +0900

    Create SumTwoNum.java
    Return the sum of the two received parameters.

---
+++ Java/SumTwoNum.java
@@ -0,0 +1,14 @@
+class Solution {
+    
+    public int solution(int num1, int num2) {
+        
+        if(-50000 <= num1 && num1 <= 50000 && -50000 <= num2 && num2 <= 50000)
+            return num1 + num2;
+        
+        return 0;
+    }
+}
+
+/*
+If the parameters num1 and num2 are within the range of -50000 to 50000, the code returns the sum of the two numbers. If they fall outside this range, it returns 0
+*/
